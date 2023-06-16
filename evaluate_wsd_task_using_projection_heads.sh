0456df51e2f92506f52ed5e9d45e3ff29e7e0d4d
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2023-06-16 19:54:04 +0900

    ci: added usage example for evaluation script.

---
+++ evaluate_wsd_task_using_projection_heads.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Try-again Mechanism disabled
+./evaluate_wsd_task_using_projection_heads.py \
+--path_output="./experiment_results/baseline.json" \
+--path_model_checkpoint="./checkpoints/baseline/last.ckpt" \
+--sense_embeddings_name="WordNet_Gloss_Corpus-AVG-bert-large-cased" \
+--eval_dataset_name="WSDEval-ALL-bert-large-cased" \
+--verbose
+
+# Try-again Mechanism enabled
+./evaluate_wsd_task_using_projection_heads.py \
+--path_output="./experiment_results/baseline.json" \
+--path_model_checkpoint="./checkpoints/baseline/last.ckpt" \
+--sense_embeddings_name="WordNet_Gloss_Corpus-AVG-bert-large-cased" \
+--eval_dataset_name="WSDEval-ALL-bert-large-cased" \
+--path_coarse_sense_inventory="./data/csi_data/wn_synset2csi.txt" \
+--try_again_mechanism \
+--verbose
