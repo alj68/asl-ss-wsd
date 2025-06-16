66889f4da3eea21f630f497075aff490f3f8c380
alj68 <alj68@cornell.edu>
2025-06-16 14:38:31 -0400

    evaluation replicated !

--- evaluate_wsd_task_using_projection_heads.sh
+++ evaluate_wsd_task_using_projection_heads.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Try-again Mechanism disabled
-./evaluate_wsd_task_using_projection_heads.py 
---path_output="./experiment_results/baseline_eval.json" 
---path_model_checkpoint="Z:\PreComputedFiles\baseline.ckpt" 
---sense_embeddings_name="WordNet_Gloss_Corpus-AVG-bert-large-cased" 
---eval_dataset_name="WSDEval-ALL-bert-large-cased" 
+./evaluate_wsd_task_using_projection_heads.py \
+--path_output="./experiment_results/baseline_eval.json" \
+--path_model_checkpoint="Z:\PreComputedFiles\baseline.ckpt"\
+--sense_embeddings_name="WordNet_Gloss_Corpus-AVG-bert-large-cased" \
+--eval_dataset_name="WSDEval-ALL-bert-large-cased" \
 --verbose
 
 # Try-again Mechanism enabled
